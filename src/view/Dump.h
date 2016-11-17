94afe448e3cf8518370dc671cd4f6936d5193fb2
Evgenii Lepikhin <johnlepikhin@gmail.com>
2016-11-17 16:32:26 +0300

    Options for 'dump' output type

--- src/view/Dump.h
+++ src/view/Dump.h
@@ -4,12 +4,20 @@
 
 #include "View.h"
 
+#include <iostream>
+#include <fstream>
+
 namespace ttop {
 namespace view {
 
 template <typename IN>
 class Dump: public View<IN> {
 public:
+	std::string OutputFile;
+	std::ofstream OutputStream;
+	std::string FieldSeparator = " ";
+	std::string RecordSeparator = "\n";
+
 	virtual std::string TypeID()
 	{
 		return ((std::string)"dump");
@@ -24,19 +32,43 @@ public:
 
 	}
 
-	virtual ~Dump() {};
+	virtual ~Dump() {
+		if (OutputStream.is_open())
+			OutputStream.close();
+	};
 
 	virtual void Output()
 	{
+		std::ostream &o = (OutputStream.is_open()) ? OutputStream : std::cout;
+
 		for (auto g_it = this->Selection.begin(); g_it!=this->Selection.end(); ++g_it) {
 			bool isFirst = true;
 			for (auto it = g_it->second.begin(); it!=g_it->second.end(); ++it) {
 				if (!isFirst)
-					std::cout << " ";
-				std::cout << (*it).Val;
+					o << FieldSeparator;
+				o << (*it).Val;
 				isFirst = false;
 			}
-			std::cout << std::endl;
+			o << RecordSeparator;
+		}
+	}
+
+	virtual void ParseParams(tinyxml2::XMLElement *node)
+	{
+		const char *_attr = node->Attribute("output");
+		if (_attr) {
+			OutputFile = _attr;
+			if ((std::string)"-" != _attr) {
+				OutputStream.open(_attr, std::ios::out | std::ios::app);
+			}
+		}
+		_attr = node->Attribute("rseparator");
+		if (_attr) {
+			RecordSeparator = _attr;
+		}
+		_attr = node->Attribute("fseparator");
+		if (_attr) {
+			FieldSeparator = _attr;
 		}
 	}
 };
