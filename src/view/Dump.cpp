121438651f4a872cbe4b660ccb64727e40579cf4
Evgenii Lepikhin <johnlepikhin@gmail.com>
2016-11-21 20:27:11 +0300

    Move Dump<> implementation to cpp

--- src/view/Dump.cpp
+++ src/view/Dump.cpp
@@ -5,5 +5,77 @@
 namespace ttop {
 namespace view {
 
+template <typename IN>
+std::string Dump<IN>::TypeID()
+{
+	return ((std::string)"dump");
+}
+
+template <typename IN>
+Dump<IN>::Dump(std::shared_ptr<logic::Logic<IN> > parser)
+	: View<IN>(parser)
+{
+
+}
+
+template <typename IN>
+Dump<IN>::~Dump() {
+	if (OutputStream.is_open())
+		OutputStream.close();
+};
+
+template <typename IN>
+void Dump<IN>::Output()
+{
+	std::ostream &o = (OutputStream.is_open()) ? OutputStream : std::cout;
+
+	for (auto g_it = this->Selection.begin(); g_it!=this->Selection.end(); ++g_it) {
+		bool isFirst = true;
+		for (auto it = g_it->second.begin(); it!=g_it->second.end(); ++it) {
+			if (!isFirst)
+				o << FieldSeparator;
+			o << (*it).Val;
+			isFirst = false;
+		}
+		o << RecordSeparator;
+	}
+}
+
+template <typename IN>
+void Dump<IN>::ParseParams(tinyxml2::XMLElement *node)
+{
+	const char *_attr = node->Attribute("output");
+	if (_attr) {
+		OutputFile = _attr;
+		if ((std::string)"-" != _attr) {
+			OutputStream.open(_attr, std::ios::out | std::ios::app);
+		}
+	}
+	_attr = node->Attribute("rseparator");
+	if (_attr) {
+		RecordSeparator = _attr;
+	}
+	_attr = node->Attribute("fseparator");
+	if (_attr) {
+		FieldSeparator = _attr;
+	}
+}
+
+template class Dump<ChunkEtherNet>;
+template class Dump<ChunkEtherNetDIX>;
+template class Dump<ChunkEtherNetSNAP>;
+template class Dump<ChunkEtherNetRAW>;
+template class Dump<ChunkEtherNet802LLC>;
+
+template class Dump<ChunkIPv4>;
+template class Dump<ChunkIPv6>;
+template class Dump<ChunkTCP>;
+template class Dump<ChunkUDP>;
+template class Dump<PacketIPv4>;
+template class Dump<PacketIPVariant>;
+template class Dump<SessionTCP>;
+template class Dump<EndPoint>;
+
+
 } /* namespace view */
 } /* namespace ttop */
--- src/view/Dump.h
+++ src/view/Dump.h
@@ -18,56 +18,11 @@ public:
 	std::string FieldSeparator = " ";
 	std::string RecordSeparator = "\n";
 
-	virtual std::string TypeID()
-	{
-		return ((std::string)"dump");
-	}
-
-	Dump(std::shared_ptr<logic::Logic<IN> > parser)
-		: View<IN>(parser)
-	{
-
-	}
-
-	virtual ~Dump() {
-		if (OutputStream.is_open())
-			OutputStream.close();
-	};
-
-	virtual void Output()
-	{
-		std::ostream &o = (OutputStream.is_open()) ? OutputStream : std::cout;
-
-		for (auto g_it = this->Selection.begin(); g_it!=this->Selection.end(); ++g_it) {
-			bool isFirst = true;
-			for (auto it = g_it->second.begin(); it!=g_it->second.end(); ++it) {
-				if (!isFirst)
-					o << FieldSeparator;
-				o << (*it).Val;
-				isFirst = false;
-			}
-			o << RecordSeparator;
-		}
-	}
-
-	virtual void ParseParams(tinyxml2::XMLElement *node)
-	{
-		const char *_attr = node->Attribute("output");
-		if (_attr) {
-			OutputFile = _attr;
-			if ((std::string)"-" != _attr) {
-				OutputStream.open(_attr, std::ios::out | std::ios::app);
-			}
-		}
-		_attr = node->Attribute("rseparator");
-		if (_attr) {
-			RecordSeparator = _attr;
-		}
-		_attr = node->Attribute("fseparator");
-		if (_attr) {
-			FieldSeparator = _attr;
-		}
-	}
+	virtual std::string TypeID();
+	Dump(std::shared_ptr<logic::Logic<IN> > parser);
+	virtual ~Dump();
+	virtual void Output();
+	virtual void ParseParams(tinyxml2::XMLElement *node);
 };
 
 } /* namespace view */
