c0193f85428cbc5dd907324d6029a9935a6f47a0
Evgenii Lepikhin <johnlepikhin@gmail.com>
2016-12-13 17:37:45 +0300

    Listen network interfaces
    '-i iface:eth0' will force ttop to listen on eth0

--- src/ttop.cpp
+++ src/ttop.cpp
@@ -14,7 +14,7 @@
 
 #include <fcntl.h>
 #include <unistd.h>
-#include <csignal>
+#include <pcap.h>
 
 std::string ttop::config::parsersXmlFile;
 std::string ttop::config::inputFile;
@@ -27,7 +27,7 @@ void parseCmdLine (int argc, char** argv)
 
 		TCLAP::ValueArg<std::string> parsersXml("c","config","Path to config file",false,"/etc/ttop/parsers.xml","file");
 
-		TCLAP::ValueArg<std::string> inputFile("i","input","Path to input file (use '-' for STDIN, this is default)",false,"-","file");
+		TCLAP::ValueArg<std::string> inputFile("i","input","Path to input file (use '-' for STDIN, this is default) or use format iface:eth0 to gather data from specified interface",false,"-","file");
 
 		TCLAP::SwitchArg helpParsers("p", "help-parsers", "Print available parsers and exit");
 
@@ -59,24 +59,55 @@ void parseCmdLine (int argc, char** argv)
 	}
 }
 
-void signal_handler(int signal) {
-	if (signal == SIGINT)
-		ttop::utils::requestExit(0);
+static const std::string iface_prefix = "iface:";
+
+void processPcapIface (ttop::AppSettings settings) {
+	std::string device = ttop::config::inputFile.substr(iface_prefix.size());
+
+	char errbuf[PCAP_ERRBUF_SIZE];
+
+	pcap_if_t *interfaces;
+
+	if(pcap_findalldevs(&interfaces, errbuf) == -1)
+		ttop::utils::fatalError("Cannot get list of interfaces: " + std::string(errbuf));
+
+	pcap_if_t *iface;
+	bool deviceFound = false;
+	for (iface = interfaces; iface; iface = iface->next) {
+		if (device == iface->name) {
+			deviceFound = true;
+			break;
+		}
+	}
+
+	if (!deviceFound)
+		ttop::utils::fatalError("Cannot find device: " + device);
+
+	pcap_t *pcap_handler = pcap_open_live(device.c_str(), 65535, 1, 0, errbuf);
+	if (!pcap_handler)
+		ttop::utils::fatalError("Cannot open device: " + device);
+
+	bool done = false;
+	while (!done) {
+		try {
+			auto InputData = util::quiltOfPcapHandler(pcap_handler);
+			settings.Parser.Recursive(InputData);
+		} catch (std::exception &exn) {
+			done = true;
+		}
+	}
+
+	pcap_close(pcap_handler);
 }
 
-int main (int argc, char** argv)
-{
-	parseCmdLine(argc, argv);
-	ttop::AppSettings settings(ttop::config::parsersXmlFile);
-
-	// TMP
-	bool done = false;
+void processFile (ttop::AppSettings settings) {
 	int FD = 0;
 
 	if (ttop::config::inputFile != "-")
 		FD = open(ttop::config::inputFile.c_str(), 0);
 
 	util::skipBytesInFD(FD, sizeof (pcap_file_header));
+	bool done = false;
 	while (!done) {
 		try {
 			auto InputData = util::quiltOfPcap(FD);
@@ -85,6 +116,19 @@ int main (int argc, char** argv)
 			done = true;
 		}
 	}
+}
+
+int main (int argc, char** argv)
+{
+	parseCmdLine(argc, argv);
+	ttop::AppSettings settings(ttop::config::parsersXmlFile);
+
+	if (!ttop::config::inputFile.compare(0, iface_prefix.size(), iface_prefix)) {
+		processPcapIface(settings);
+	} else {
+		processFile(settings);
+	}
 
 	ttop::utils::requestExit(0);
+	return (0);
 }
